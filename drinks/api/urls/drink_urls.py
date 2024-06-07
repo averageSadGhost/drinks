9fb382a36a819ec233f445480cf088f13ae140c8
mohamedAli04Academy <151844430+mohamedAli04Academy@users.noreply.github.com>
2024-06-07 14:14:00 +0300

    finished project

--- drinks/api/urls/drink_urls.py
+++ drinks/api/urls/drink_urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from ..views import drink_views as views
 
 urlpatterns = [
-    path('drinks/', views.drink_list, name='drink_list'),
     path('drinks/<int:id>/', views.drink_detail, name='drink_detail'),
+    path('drinks/', views.drink_list, name='drink_list'),
 ]
